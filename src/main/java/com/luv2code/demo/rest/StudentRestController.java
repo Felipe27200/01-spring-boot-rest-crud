0ed0b2505e656ae0faeb0ccb4bf892cac4dba8b4
juan.zea <juan.zea@innclod.com>
2023-09-25 23:10:44 -0500

    Define route params and work with them

--- src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,22 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController
 {
+    private List<Student> theStudents;
+
+    // Define @PostConstructor to load the data... only once!
+    // When this is being constructed.
+    @PostConstruct
+    public void loadData()
+    {
+        // Define a List of students and assign an ArrayList
+        theStudents = new ArrayList<>();
+
+        // Add the students
+        theStudents.add(new Student());
+        theStudents.add(new Student("Mario", "Rossi"));
+        theStudents.add(new Student("Mary", "Smith"));
+    }
+
     /*
     * +--------------------------------------+
     * | DEFINE THE ENDPOINT TO RETURN A LIST |
@@ -27,14 +45,31 @@ public class StudentRestController
     @GetMapping("/students")
     public List<Student> getStudents()
     {
-        // Define a List of students and assign an ArrayList
-        List<Student> students = new ArrayList<>();
+        return theStudents;
+    }
 
-        // Add the students
-        students.add(new Student());
-        students.add(new Student("Mario", "Rossi"));
-        students.add(new Student("Mary", "Smith"));
-
-        return students;
+    /*
+    * +---------------------------+
+    * | SEND PARAMS FOR THE ROUTE |
+    * +---------------------------+
+    *
+    * The params on the route are surround
+    * by curly braces {}, and the name of
+    * the param inside of them.
+    *
+    * Then we define a parameter in the method
+    * to take the value of the route param and both
+    * must have the same name, exactly.
+    * */
+    @GetMapping("/students/{studentId}")
+    /*
+    * The @PathVariable put the route param value inside
+    * the method param that match with name of the first.
+    * */
+    public Student getStudent(@PathVariable int studentId)
+    {
+        // We are using the get() method of the List<>
+        // to get the object with the index of the element.
+        return this.theStudents.get(studentId);
     }
 }
