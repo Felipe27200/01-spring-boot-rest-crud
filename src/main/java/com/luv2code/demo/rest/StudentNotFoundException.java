7fcdf855638306ab69354289bd78eb1133f49b1f
juan.zea <juan.zea@innclod.com>
2023-09-26 21:33:00 -0500

    How to manage exceptions in the request

---
+++ src/main/java/com/luv2code/demo/rest/StudentNotFoundException.java
@@ -0,0 +1,20 @@
+package com.luv2code.demo.rest;
+
+public class StudentNotFoundException extends RuntimeException
+{
+    // This class is calling the parent constructor with super();
+
+    public StudentNotFoundException (String message)
+    {
+        super(message);
+    }
+
+    public StudentNotFoundException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+    public StudentNotFoundException(Throwable cause)
+    {
+        super(cause);
+    }
+}
